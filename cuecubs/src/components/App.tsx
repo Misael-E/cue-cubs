31020a4e3f0dd8bbe447a7f748ddb818efd34c55
Misael-E <misael.esperanzate@hotmail.com>
2023-10-08 16:59:54 -0600

    start screen

---
+++ cuecubs/src/components/App.tsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+const App = () => {
+	return (
+		<>
+			<div className="start-btn-container">
+				<button className="start-btn">Let's Start!</button>
+			</div>
+		</>
+	);
+};
+
+export default App;
